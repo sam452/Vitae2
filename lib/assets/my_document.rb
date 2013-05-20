a22412e081f226b559688e646bab0dfb51bf2e4f
sam walton <sam@foofrancine.com>
2013-05-20 13:23:52 -0500

    pdf use columns, pdf icon

--- lib/assets/my_document.rb
+++ lib/assets/my_document.rb
@@ -1,6 +1,6 @@
 class MyDocument < Prawn::Document
     def initialize(opening, view)
-      super(top_margin: 36, keywords: "Sam Walton Rails developer",
+      super(top_margin: 36, left_margin: 26, right_margin: 26, keywords: "Sam Walton Rails developer",
       	    creator: "samwalton.me", CreationDate: "#{Time.now}")
       #encoding: utf-8
       @opening = opening
@@ -11,10 +11,10 @@ class MyDocument < Prawn::Document
       	:light => Dir.pwd + "/app/assets/stylesheets/Nunito-Light.ttf"
       })
       font "Nunito"
-      text "Sam Walton", style: :bold
-      text "7540 Harper Rd"
-      text "Joelton, TN 37080, near Nashville, TN"
-      text "615-495-7783 \u2022 sam@samwalton.me"
+      text "Sam Walton", style: :bold, leading: -1
+      text "7540 Harper Rd", leading: -1
+      text "Joelton, TN 37080, near Nashville, TN", leading: -1
+      text "615-495-7783 \u2022 sam@samwalton.me", leading: -1
 
 	bounding_box([350, 674], width: 72) do
 	  text "<color rgb='cb2e03'><u><link href='http://www.linkedin.com/in/samwalton0'>Linkedin</link></u></color>", :inline_format => true
@@ -25,13 +25,10 @@ class MyDocument < Prawn::Document
       image "#{Dir.pwd + '/app/assets/images/linkedin.png'}", at: [350, 720]
       image "#{Dir.pwd + '/app/assets/images/github_logo.png'}", at: [415, 720]
       move_down 6
-      subhed "Career Objective"
-      text "#{opening.objective}" 
-      move_down 6
       subhed "Experience"
 move_up 12
 
- 			my_skills = [["\u2022 Cucumber, Rspec and unit test-driven development", "\u2022 Written several Applescript applets for automation"], ["\u2022 Ruby on Rails, Ruby", "\u2022 Set up several servers at work, home"], ["\u2022 Javascript, CSS, HTML5", "\u2022 Translating team requirements for quality, throughput"], ["\u2022 Wordpress theme creation", "\u2022 Extensive management experience"], ["\u2022 Subject-matter expert on Adobe CS print apps", "\u2022 Comfortable speaking before groups"], ["\u2022 Filemaker Pro, other SQL experience"]]
+ 			#my_skills = [["\u2022 Cucumber, Rspec and unit test-driven development", "\u2022 Written several Applescript applets for automation"], ["\u2022 Ruby on Rails, Ruby", "\u2022 Set up several servers at work, home"], ["\u2022 Javascript, CSS, HTML5", "\u2022 Translating team requirements for quality, throughput"], ["\u2022 Wordpress theme creation", "\u2022 Extensive management experience"], ["\u2022 Subject-matter expert on Adobe CS print apps", "\u2022 Comfortable speaking before groups"], ["\u2022 Filemaker Pro, other SQL experience"]]
  
       current
       tight_right "Grizzard Inc., Atlanta, Ga."
@@ -41,7 +38,10 @@ move_up 12
       tight_right "The Union-Recorder, Milledgeville, Ga."
       base "News Editor of Page 1A and related news pages for this 8,5000-circulation five-day morning Knight-Ridder newspaper from <b>1993 to 1994</b>. To get to this position, I taught myself to use the new Macintosh and PC software to advance the newspaper to pagination integrating text, artwork and photos. This was an outgrowth of my career as a photojournalist from <b>1986</b>."
       subhed "Skills"
- 			table(my_skills, column_widths: [270, 270], cell_style: {padding: [0, 3, 0, 0], borders: []})
+ 			#table(my_skills, column_widths: [270, 270], cell_style: {padding: [0, 3, 0, 0], borders: []})
+      column_box([0, cursor], columns: 2, width: bounds.width, height: 115) do
+      	text("\u2022 Cucumber, Rspec and unit test-driven development \n\u2022 Ruby on Rails, Ruby\n\u2022 Javascript, CSS, HTML5\n\u2022 Wordpress theme creation\n\u2022 Subject-matter expert on Adobe CS print apps\n\u2022 Filemaker Pro, other SQL experience\n\u2022 Written several Applescript applets for automation\n\u2022 Set up several servers at work, home\n\u2022 Translating team requirements for quality, throughput\n\u2022 Extensive management experience\n\u2022 Comfortable speaking before groups")
+      end
       subhed "Education"
       text "\u2022 Nashville Software School, 2012."
       text "\u2022 Pragmatic Studio for Ruby on Rails, January 2009."
@@ -73,7 +73,7 @@ move_up 12
 
     def base(text)
     	self.indent(12) do 
-    		self.text text, style: :normal, inline_format: true
+    		self.text text, style: :normal, inline_format: true, leading: -1
     	end
     end
 
